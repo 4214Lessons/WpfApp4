016175bbc85c77065b8d97a96af43d9fc54f9bbe
Tural Novruzov <t.novruzov@itstep.edu.az>
2022-10-24 18:03:00 +0400

    Add project files.

---
+++ Source/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Bogus;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Source;
+
+public partial class MainWindow : Window
+{
+    public List<User> Users { get; set; }
+
+
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        DataContext = this;
+
+
+
+        // Bogus > Faker (Fake data)
+
+        Users = new Faker<User>()
+            .RuleFor(u => u.Firstname, faker => faker.Person.FirstName)
+            .RuleFor(u => u.Lastname, faker => faker.Person.LastName)
+            .RuleFor(u => u.Gender, faker => faker.Person.Gender.ToString())
+            .RuleFor(u => u.Phone, faker => faker.Person.Phone)
+            .RuleFor(u => u.ImageUrl, faker => faker.Person.Avatar)
+            .RuleFor(u => u.Email, faker => faker.Person.Email)
+            .RuleFor(u => u.Address, faker => faker.Person.Address.City)
+            .RuleFor(u => u.DateOfBirth, faker => faker.Person.DateOfBirth)
+            .Generate(10);
+    }
+
+
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Button btn = sender as Button;
+        Grid grid = btn.Parent as Grid;
+        StackPanel sp = grid.Children[1] as StackPanel;
+        TextBlock textBlock = sp.Children[0] as TextBlock;
+
+        MessageBox.Show(textBlock.Text);
+    }
+
+
+
+    private void AddUser(object sender, RoutedEventArgs e)
+    {
+        listView.ItemsSource = null;
+        Users.Add(new User { Firstname = "AAA" });
+        listView.ItemsSource = Users;
+    }
+}
---
+++ Source/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Source;
+
+
+public class User
+{
+    public Guid Id { get; } = Guid.NewGuid();
+
+    public string? Firstname { get; set; }
+    public string? Lastname { get; set; }
+    public string? Gender { get; set; }
+    public string? Phone { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? Email { get; set; }
+    public string? Address { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+
+    public override string ToString()
+     => @$"{Firstname} - {Lastname} - {Gender} - {Phone} - {Email} - {Address}";
+}
